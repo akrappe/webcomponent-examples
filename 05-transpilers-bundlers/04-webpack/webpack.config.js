d0c42aa490ffed100f10d861ebd1c8ff2207270d
Jan Rembold <janrembold@googlemail.com>
2018-03-06 07:09:23 +0100

    added webpack demo

---
+++ 05-transpilers-bundlers/04-webpack/webpack.config.js
@@ -0,0 +1,39 @@
+const {resolve} = require('path');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+module.exports = {
+    context: resolve(__dirname, 'src'),
+    entry: {
+        "custom-component": './custom-component.js',
+        "custom-component.min": './custom-component.js'
+    },
+    output: {
+        filename: '[name].js',
+        path: resolve(__dirname, 'dist'),
+    },
+    resolve: {
+        extensions: ['.js']
+    },
+    devtool: 'source-map',
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    },
+    plugins: [
+        new UglifyJSPlugin({
+            include: /\.min\.js$/
+        })
+        // ,new BundleAnalyzerPlugin()
+    ]
+};
